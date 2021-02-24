0858431eeff4a74beec6f7eb15b557089517116a
flrizzato <fernando.rizzato@gmail.com>
2021-02-24 19:02:53 -0300

    RAD Studio Live 2021 Portuguese

---
+++ 2021/Portuguese/09-UX UI avancadas comps nativos/Unit1.pas
@@ -0,0 +1,57 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls,
+  Vcl.Imaging.pngimage;
+
+type
+  TForm1 = class(TForm)
+    Panel1: TPanel;
+    Panel2: TPanel;
+    Panel3: TPanel;
+    Panel4: TPanel;
+    Panel5: TPanel;
+    Panel6: TPanel;
+    Label1: TLabel;
+    Edit1: TEdit;
+    Panel8: TPanel;
+    Panel9: TPanel;
+    Label3: TLabel;
+    Edit3: TEdit;
+    Panel10: TPanel;
+    Panel7: TPanel;
+    CheckBox2: TCheckBox;
+    Image1: TImage;
+    Image2: TImage;
+    Image3: TImage;
+    Panel11: TPanel;
+    Panel12: TPanel;
+    Panel13: TPanel;
+    Image4: TImage;
+    Panel14: TPanel;
+    Panel15: TPanel;
+    Label4: TLabel;
+    Panel16: TPanel;
+    Label5: TLabel;
+    Panel17: TPanel;
+    Label6: TLabel;
+    Image5: TImage;
+    Panel18: TPanel;
+    Label2: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
